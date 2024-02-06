fcce5dc8b2348a755457defc7297fa797b786dc5
Gabriel Samuel <samuelgab246@gmail.com>
2024-02-06 10:55:58 -0800

    advanced JS

---
+++ script.js
@@ -0,0 +1,8 @@
+const name = 'Sally';
+const age = 34;
+const pet = "horse";
+
+const greeting = "Hello" + name + "you seem to be doing" + greeting + "!";
+
+const greetingBest = `Hello ${name} you seem to be ${age-10}. What a lovely ${pet} you have`;
+
